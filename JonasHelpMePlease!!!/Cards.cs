4aeec9a15b9b5cec0b06b217c3308c0388d04c0d
lefex-v2 <146823569+lefex-v2@users.noreply.github.com>
2023-11-05 17:09:54 +0100

    Done with higher or lower!

---
+++ JonasHelpMePlease!!!/Cards.cs
@@ -0,0 +1,33 @@
+﻿using System.Transactions;
+using System.Collections;
+using System.Drawing;
+using System.ComponentModel.DataAnnotations;
+
+
+class Cards
+{
+    public int cardValue;
+    public string cardName;
+    public string cardSuit; 
+
+    public Cards(string Name, int value, string suit)
+    {
+        this.cardValue = value;
+        this.cardName = Name;
+        this.cardSuit = suit;
+    }
+
+    public bool CompareValue(Cards card)
+    {
+        //ture = the card that is runing is lagger and if false then the card that was pasted was lagger!
+        if (this.cardValue >= card.cardValue)
+        {
+            return true;
+        }
+        else if (this.cardValue <= card.cardValue)
+        {
+            return false;
+        }
+        return false;
+    }
+}
---
+++ JonasHelpMePlease!!!/MainGame.cs
@@ -0,0 +1,64 @@
+﻿
+using System.Net.Http.Headers;
+
+Deck allCards = new Deck();
+
+Console.WriteLine("This game is a higher or lower card game!");
+Console.WriteLine("You can pick higher or lower with H and L! (Q to quit!) \r\n");
+
+allCards.ShuffleDeck();
+
+int points = 0;
+int cinx = 0;
+string HorL = string.Empty;
+
+while (true)
+{ 
+    Console.WriteLine(allCards.deck[cinx].cardName + "\r\n");
+    Console.WriteLine("Higher or lower?");
+
+    bool loop = true;
+    while (loop == true)
+    {
+        string d = Console.ReadLine();
+        if (d == "H" || d == "h" || d == "l" || d == "L")
+        {
+            HorL = d;
+            loop = false;
+        }
+        if(d == "Q" || d == "q")
+        {
+            Environment.Exit(0);
+        }
+    }
+    Console.Clear();
+
+    bool yes = allCards.deck[cinx + 1].CompareValue(allCards.deck[cinx]);
+
+    if(yes && (HorL == "h" || HorL == "H"))
+    {
+        points++;
+        Console.WriteLine("You where right! The card was {0}, and it was agents {1}.", allCards.deck[cinx].cardName, allCards.deck[cinx+1].cardName);
+        Console.WriteLine("You have " + points + " points!");
+    }
+    else if (!yes && (HorL == "l" || HorL == "L"))
+    {
+        points++;
+        Console.WriteLine("You where right! The card was {0}, and it was agents {1}.", allCards.deck[cinx].cardName, allCards.deck[cinx + 1].cardName);
+        Console.WriteLine("You have " + points + " points!");
+    }
+    else
+    {
+        Console.WriteLine("That was wrong! The card was {0}, and it was agents {1}.", allCards.deck[cinx].cardName, allCards.deck[cinx + 1].cardName);
+    }
+    cinx += 2;
+
+    if (cinx >= allCards.deck.Count-1)
+    {
+        Console.WriteLine("The game is over!!!!11!1!");
+        Console.WriteLine("You have " + points + " points!");
+        break;
+    }
+}
+Console.WriteLine("Press enter to exit!");
+Console.ReadLine();
